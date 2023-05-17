2212047e2abaa0d150f4449004d4ae2b18463f9e
DriveFallen <kobtsev2003@mail.ru>
2023-05-17 03:24:36 +0300

    first commit

---
+++ prog4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace prog4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void var1(double x, double y, double n, double h)
+            {
+                for (int i = 1; x < n; x += h, i++)
+                {
+                    y += h * y;
+                    Console.WriteLine("x" + i + " = " + Math.Round(x, 6) + "; y = " + Math.Round(y, 6));
+                }
+                Console.WriteLine();
+            }
+            var1(0, 1, 1, 0.1);
+            Console.ReadLine();
+        }
+    }
+}
